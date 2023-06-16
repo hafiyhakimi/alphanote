92cb9405b05549dd00452283ed09b5a8939566a9
hafiyhakimi <hafiyhakimi1665@gmail.com>
2023-06-16 20:06:45 +0800

    updated for radin

--- lib/sidebarwidget.dart
+++ lib/sidebarwidget.dart
@@ -1,131 +1,10 @@
-// import 'package:flutter/material.dart';
-// import 'notetaking/ui/notemain.dart';
-// import 'toDolist/toDoList.dart';
-// import 'calendar/calendar_main.dart';
-// import 'main.dart';
-
-// class SidebarWidget extends StatelessWidget {
-//   const SidebarWidget({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Drawer(
-//       child: Column(
-//         children: [
-//           Container(
-//             height: 200,
-//             width: double.infinity,
-//             decoration: BoxDecoration(
-//               color: Colors.blue,
-//               border: Border(
-//                 bottom: BorderSide(
-//                   color: Colors.white,
-//                   width: 2,
-//                 ),
-//               ),
-//             ),
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 CircleAvatar(
-//                   backgroundImage: AssetImage('assets/pfp.jpeg'),
-//                   radius: 50,
-//                 ),
-//                 SizedBox(height: 10),
-//                 Text(
-//                   'John Doe',
-//                   style: TextStyle(
-//                     color: Colors.white,
-//                     fontSize: 20,
-//                   ),
-//                 ),
-//                 SizedBox(height: 5),
-//                 Text(
-//                   'johndoe@example.com',
-//                   style: TextStyle(
-//                     color: Colors.white,
-//                     fontSize: 16,
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//           SizedBox(height: 10),
-//           ListTile(
-//             leading: Icon(Icons.home),
-//             title: Text('Dashboard'),
-//             onTap: () {
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(builder: (context) => MyApp()),
-//               );
-//             },
-//           ),
-//           ListTile(
-//             leading: Icon(Icons.check),
-//             title: Text('To-do List'),
-//             onTap: () {
-//               // TODO: implement action for Option 2
-
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(builder: (context) => TodoList()),
-//               );
-//             },
-//           ),
-//           ListTile(
-//             leading: Icon(Icons.note),
-//             title: Text('Notes'),
-//             onTap: () {
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(builder: (context) => NoteMain()),
-//               );
-//             },
-
-//           ),
-//           ListTile(
-//             leading: Icon(Icons.calendar_today),
-//             title: Text('Calendar'),
-//             onTap: () {
-//               // TODO: implement action for Option 4
-
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(builder: (context) => calendar_main()),
-//               );
-//             },
-//           ),
-//           ListTile(
-//             leading: Icon(Icons.settings),
-//             title: Text('Settings'),
-//             onTap: () {
-//               // TODO: implement action for Option 5
-//             },
-//           ),
-//           Spacer(),
-//           Container(
-//             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-//             child: Text(
-//               'Version 1.0.0',
-//               style: TextStyle(
-//                 color: Colors.grey[600],
-//                 fontSize: 14,
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'notetaking/ui/notemain.dart';
-import 'toDolist/toDoList.dart';
+import 'todolist/providers/todo_provider.dart';
+import 'todolist/toDolist.dart';
 import 'main.dart';
 import 'notetaking/ui/notemain2.dart';
 import 'authprovider.dart';
@@ -194,9 +73,14 @@ class SidebarWidget extends StatelessWidget {
             leading: Icon(Icons.check),
             title: Text('To-do List'),
             onTap: () {
+              // TODO: implement action for Option 2
+
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => TodoList()),
+                MaterialPageRoute(builder: (context) => ChangeNotifierProvider(
+                    create: (context) => TodoProvider(),
+                    builder: (context, child) => const TodoList()
+                )),
               );
             },
           ),
